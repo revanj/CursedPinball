f6f87c74056829ad393516c1ba858e3669178c13
Roger Wang <rogerwang2520@gmail.com>
2023-04-28 22:46:22 -0500

    Create ball and bucket prefabs

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    [SerializeField] int ballsNeeded = 1;
+    int ballsLeft;
+
+    void Awake()
+    {
+        ballsLeft = ballsNeeded;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<Ball>(out Ball ball))
+        {
+            return;
+        }
+        ballsLeft--;
+        ballsLeft = Mathf.Max(0, ballsLeft);
+    }
+
+    public bool IsFull()
+    {
+        return ballsLeft <= 0;
+    }
+}
