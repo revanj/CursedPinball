223cb29da1be83426ffb9b0b4f7f1de7e7497db9
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-05-01 18:04:16 -0500

    implement tips UI and UI animation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -11,6 +11,7 @@ public class Ball : MonoBehaviour
     public ColorSO colorSO;
     public static GameObject ballPrefab;
     public static GameObject ballDiscretePrefab;
+    private float time = 0;
     void Awake() {
         if(colorSO) Init(colorSO);
     }
@@ -25,6 +26,16 @@ public class Ball : MonoBehaviour
         if(Utils.IsGameObjectOutOfScreenBoundsNotTop(this.gameObject, GameManager.Instance.mainCamera)){
             Destroy(this.gameObject);
             GameManager.Instance.TryChangeToLoseState();
+        }
+        if(GetComponent<Rigidbody2D>().velocity.magnitude < 0.1f){
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            time += Time.deltaTime;
+        }
+        else{
+            time = 0f;
+        }
+        if(time > 3f){
+            UIManager.Instance.ShowRestartTip();
         }
     }
     public static Ball CreateBall(ColorSO colorSO, bool UseDiscreteRigidbody2d)
