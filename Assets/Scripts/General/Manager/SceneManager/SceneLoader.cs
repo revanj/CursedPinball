9aae4871c30fa852a491e6b90a7447b8038518a9
Revan Ji <jiruifancr@gmail.com>
2023-05-01 16:47:56 -0500

    Add correct transition from title to scene selection

--- Assets/Scripts/General/Manager/SceneManager/SceneLoader.cs
+++ Assets/Scripts/General/Manager/SceneManager/SceneLoader.cs
@@ -6,8 +6,32 @@ public class SceneLoader : MonoBehaviour
     [SerializeField] 
     private SceneReference _sceneToLoad;
 
+    [SerializeField] private bool PreservePreviousBGM;
+
     public void LoadScene()
     {
+        if (PreservePreviousBGM)
+        {
+            DontDestroyOnLoad(GameObject.FindWithTag("Music"));
+        }
+        else
+        {
+            SceneManager.MoveGameObjectToScene(GameObject.FindWithTag("Music"), SceneManager.GetActiveScene());
+        }
         SceneManager.LoadScene(_sceneToLoad);
+    }
+
+    public void LoadSceneNoPreserveBGM()
+    {
+        BGM.PreservePrevious = false;
+        PreservePreviousBGM = false;
+        LoadScene();
+    }
+
+    public void LoadScenePreserveBGM()
+    {
+        BGM.PreservePrevious = true;
+        PreservePreviousBGM = true;
+        LoadScene();
     }
 }
